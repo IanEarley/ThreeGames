58c15d2c11a0b769bd12fc9a885447f25508a9ce
IanEarley <iearley88@gmail.com>
2023-04-18 19:15:54 +0000

    Initial commit

---
+++ VariablesLesson.py
@@ -0,0 +1,14 @@
+#Variables can be integers, floats (decimals), strings or booleans
+age = 20
+temperature = 25.39
+country = "Australia"
+is_day = False
+
+print("That girl is " + str(age) + " years old and she's from " + country + "!")
+
+print("You want to know if it's light outside? Let me check...")
+
+if is_day == True:
+  print("Looks like it's still day out.")
+else:
+  print("Oof looks like the sun went down.")
